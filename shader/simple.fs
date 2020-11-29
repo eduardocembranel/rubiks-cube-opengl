67ba2d6fa41f1face742131e06644530f90280fb
Eduardo Cembranel <eduardocembranel@outlook.com>
2020-11-29 14:12:08 -0300

    frag updated

--- shader/simple.fs
+++ shader/simple.fs
@@ -27,30 +27,43 @@ in vec2 texCoord;
 
 out vec4 FragColor;
 
+vec3 getAmbient() {
+    if (!material.hasTexture) {
+        return light.ambient * material.ambient;
+    } else {
+        return light.ambient * vec3(texture(material.diffuseTexture, texCoord));
+    }
+}
+
+vec3 getDiffuse(float diff) {
+    if (!material.hasTexture) {
+        return light.diffuse * (diff * material.diffuse);
+    } else {
+        return light.diffuse * diff * vec3(texture(material.diffuseTexture, texCoord));
+    }
+}
+
+vec3 getSpecular(float spec) {
+    return light.specular * (spec * material.specular);
+}
+
 void main()
 {   
+    //ambient
+    vec3 ambient = getAmbient();
 
+    //diffuse
     vec3 norm = normalize(normal);
     vec3 lightDir = normalize(light.position - pos);
     float diff = max(dot(norm, lightDir), 0.0);
-
-    vec3 ambient;
-    vec3 diffuse;
-    if (!material.hasTexture) {
-        ambient = light.ambient * material.ambient;
-        diffuse = light.diffuse * (diff * material.diffuse);
-    } else {
-        ambient = light.ambient * vec3(texture(material.diffuseTexture, texCoord));
-        diffuse = light.diffuse * diff * vec3(texture(material.diffuseTexture, texCoord));
-    }
+    vec3 diffuse = getDiffuse(diff);
 
     // specular
     vec3 viewDir = normalize(cameraPos - pos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular);  
+    vec3 specular = getSpecular(spec);
 
-    vec3 result = ambient + diffuse + specular;
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(ambient + diffuse + specular, 1.0);
 
 }
